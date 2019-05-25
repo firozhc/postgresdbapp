fd8f4976cf4f9672538d51a3e5307e7d3a381f8c
Firoz Hossain Chaudhuri <emperorscourge@gmail.com>
2019-05-25 19:03:33 -0400

    second commit

--- dbreader.py
+++ dbreader.py
@@ -11,6 +11,7 @@ import csv
 import shutil
 import datetime
 import psycopg2
+import re
 
 
 # This function will read the csv file and extract the header
@@ -18,28 +19,80 @@ def readheader(filename, delim):
 	
 	try:
 
-		out_dir = '/users/firoz/desktop/outfile.csv'
-		
-		file = open(filename, 'r')
+		if delim == "t":
+			delim = '\t'
 
-		delim = '\t'
+		file = open(filename, 'r')
 
 		csv_reader = csv.reader(file, delimiter=delim, quoting=csv.QUOTE_MINIMAL)
 	
 		headers = next(csv_reader, None)
 
-		sql_head = "create table "+ filename +" ("
+		pattern = r'/([a-zA-Z0-9_-]*)\.(.*)'
+
+		match = re.search(pattern,filename)
+
+		if match:
+			fname = match.group(1)
+
+		sql_head = "create table "+ fname +" ("
 
 		sql_body = " varchar,".join(headers) +" varchar"
 
 		sql_stmt = sql_head + sql_body + " )"
 
-		print(sql_stmt)
+		return (sql_stmt, filename, delim)
 
 
 	except Exception as e:
 		print("Error in Reading File "+ filename + str(e))
 
+
+def db_conn(create_table_sql, filename, delimiter):
+
+	try:
+		print(delimiter)
+		pattern = r'/([a-zA-Z0-9_-]*)\.(.*)'
+		match = re.search(pattern,filename)
+		if match:
+			fname = match.group(1)
+
+		connection = psycopg2.connect(user = "postgres", password = "x", host = "127.0.0.1", port = "5432", database = "postgres")
+		cursor = connection.cursor()
+		cursor.execute(create_table_sql)
+		connection.commit()
+		print("\nTable "+fname+" Created Successfully!")
+
+		select_sql = "Select * from "+fname+" limit 10"
+
+		cursor.execute(select_sql)
+
+		c = cursor.fetchall()
+
+		print(c)
+		
+
+		copy_sql = "COPY "+ fname+ " FROM '"+ filename+ "' DELIMITER E'"+ delimiter+"'"+ "CSV HEADER QUOTE '\"' ESCAPE '\"' ENCODING 'LATIN1' ;"
+		print('\n'+copy_sql)
+
+		cursor.execute(copy_sql)
+
+
+		select_sql = "Select * from "+fname+" limit 10"
+
+		cursor.execute(select_sql)
+
+		c = cursor.fetchall()
+
+		print(c)
+
+	except (Exception, psycopg2.Error) as error :
+		print ("Error while connecting to PostgreSQL", error)
+	finally:#closing database connection.
+		if(connection):
+			cursor.close()
+			connection.close()
+			print("PostgreSQL connection is closed")
 
 
 
@@ -47,15 +100,17 @@ def main():
 
 	print("Please Enter the File Name: ")
 
-	filename = input()
+	filename = str(input().strip())
 
 	print("Please Enter the File Delimiter: ")
 
-	#delim = str(input().strip())
+	delim = str(input().strip())
 
-	#print(delim)
+	tup = readheader(filename, delim)
 
-	readheader(filename, 'tab')
+	print(tup)
+
+	db_conn(tup[0],tup[1],tup[2])
 	
 
 
